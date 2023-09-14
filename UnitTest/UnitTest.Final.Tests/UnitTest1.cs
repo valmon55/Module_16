60c103a89209c3e00aeca21c16696b8b300bd3c4
valmon55 <fedor_ka@mail.ru>
2023-09-14 12:20:09 +0300

    start

---
+++ UnitTest/UnitTest.Final.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UnitTest.Final.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
