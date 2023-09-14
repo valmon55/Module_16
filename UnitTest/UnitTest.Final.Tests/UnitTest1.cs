306675ca657adf3975069e88b3732da35f3de743
valmon55 <fedor_ka@mail.ru>
2023-09-14 12:32:07 +0300

    готово

--- UnitTest/UnitTest.Final.Tests/UnitTest1.cs
+++ UnitTest/UnitTest.Final.Tests/UnitTest1.cs
@@ -2,15 +2,39 @@ namespace UnitTest.Final.Tests
 {
     public class Tests
     {
+        Calculator calc = new Calculator();
+
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void AdditionTest()
         {
-            Assert.Pass();
+            Assert.AreEqual(8, calc.Additional(3, 5));
+            //Assert.Pass();
         }
+        [Test]
+        public void SubtractionTest()
+        {
+            Assert.AreEqual(-2, calc.Subtraction(3, 5));
+        }
+        [Test]
+        public void MiltiplicationTest()
+        {
+            Assert.AreEqual(15, calc.Miltiplication(3, 5));
+        }
+        [Test]
+        public void DivisionTest()
+        {
+            Assert.That(calc.Division(35, 5) == 7);
+        }
+        [Test]
+        public void DivisionByZeroEceptionTest()
+        {
+            Assert.Throws<DivideByZeroException>(() => calc.Division(3, 0));
+        }
+
     }
 }
